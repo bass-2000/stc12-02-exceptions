0ff09f14a6b8e29f0598626adb05ae9a31b67a79
bass-2000 <bass-2000@yandex.ru>
2018-08-21 10:50:21 +0300

    editing Exception throwing

--- src/ru/innopolis/stc12/exceptions/MathBox.java
+++ src/ru/innopolis/stc12/exceptions/MathBox.java
@@ -1,17 +1,29 @@
 package ru.innopolis.stc12.exceptions;
 
-import java.io.IOException;
 
 public class MathBox {
-
+    /**
+     * @param a
+     * @param b
+     * @return summary of params
+     */
     public int summa(Integer a, Integer b) {
         return a + b;
     }
 
+    /**
+     * @param a
+     * @param b
+     * @return difference between two params
+     */
     public int diff(Integer a, Integer b) {
         return a - b;
     }
 
+    /**
+     * @param value
+     * @return factorial of value
+     */
     public long factorial(Integer value) {
         if (value == 1 || value == 0) {
             return 1;
@@ -19,42 +31,50 @@ public class MathBox {
         return factorial(value - 1) * value;
     }
 
+    /**
+     * @param a
+     * @param b
+     * @return dividing param a by b with catching Arithmetic Exception
+     */
     public double dividerExceptionInside(Integer a, Integer b) {
-        try{
-            return a/b;
-        }catch(ArithmeticException e){
-            System.out.println("Catched Exception "+ e);
+        try {
+            return a / b;
+        } catch (ArithmeticException e) {
+            System.out.println("Catched Exception " + e);
         }
         return 0;
     }
 
-    public double divider(Integer a, Integer b) throws Exception {
+    /**
+     * @param a
+     * @param b
+     * @return dividing param a by b and throw new Exception as custom exception
+     * @throws Exception
+     */
+    public double divider(Integer a, Integer b) throws MyCustomException {
         try {
             return a / b;
-        }catch(ArithmeticException e){
-            throw new Exception(e.getMessage() +" was catched and thrown");
+        } catch (ArithmeticException e) {
+            throw new MyCustomException(e.getMessage() + " was catched and thrown as custom Exception");
         }
     }
 
-    public static void main(String[] args) throws MyCustomException {
+    public static void main(String[] args) throws Exception {
         MathBox mathBox = new MathBox();
 
         System.out.println("Method summa: " + mathBox.summa(1, 3));
         System.out.println("\nMethod diff: " + mathBox.diff(1, 3));
-        try{
+        try {
             System.out.println("\nMethod factorial: ");
             System.out.println(mathBox.factorial(Integer.MAX_VALUE));
-        }catch (StackOverflowError e){
-            System.out.println("Catched Exception " +e);
+        } catch (StackOverflowError e) {
+            System.out.println("Catched Exception " + e);
         }
         System.out.println("\nMethod dividerExceptionInside: ");
-        System.out.println(mathBox.dividerExceptionInside(13,0));
+        System.out.println(mathBox.dividerExceptionInside(13, 0));
         System.out.println("\nMethod divider throwing exception: ");
-        try {
-            System.out.println(mathBox.divider(26, 0));
-        }catch (Exception e){
-            throw new MyCustomException("My custom Exception, with info : " + e );
-        }
+        System.out.println(mathBox.divider(26, 0));
+
     }
 
 }
